a8d3682dffd5ebdc8af7ef06545eaecc711ae89d
Guilherme Silva <guilhermecvt@hotmail.com>
2024-08-23 07:56:16 -0300

    Create Comment With Author Presenter

--- src/infra/http/controllers/fetch-question-comments.controller.ts
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Controller, Get, Param, Query } from "@nestjs/comm
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe";
 import { z } from "zod";
 import { FetchQuestionCommentsUseCase } from "@/domain/forum/application/use-cases/fetch-question-comments";
-import { CommentPresenter } from "../presenters/comment-presenter";
+import { CommentWithAuthorPresenter } from "../presenters/comment-with-author-presenter";
 
 const pageQueryParamSchema = z
   .string()
@@ -36,6 +36,6 @@ export class FetchQuestionCommentsController {
 
     const comments = result.value.comments
 
-    return { comments: comments.map(CommentPresenter.toHTTP) }
+    return { comments: comments.map(CommentWithAuthorPresenter.toHTTP) }
   }
 }
